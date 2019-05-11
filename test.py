b5ee0c52e957ffb18f01993e63748aaad583823f
Alessandro Mamusa <alesardo@develer.com>
2019-05-11 16:46:57 +0200

    add test for tdd game of life

---
+++ test.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+GAME OF LIFE TEST SUIT
+
+rules:
+- If a cell is ON and has fewer than two neighbors that are ON, it
+turns OFF.
+- If a cell is ON and has either two or three neighbors that are ON,
+it remains ON.
+- If a cell is ON and has more than three neighbors that are ON, it
+turns OFF.
+- If a cell is OFF and has exactly three neighbors that are ON, it
+turns ON.
+"""
