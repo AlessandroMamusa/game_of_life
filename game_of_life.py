c6b5a1989ed66dfe0b24e6c4dca57fae3913eb5b
Alessandro Mamusa <alesardo@develer.com>
2019-05-12 20:26:31 +0200

    neighbors implementantion

---
+++ game_of_life.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Game(object):
+    """
+    rules:
+    - If a cell is ON and has fewer than two neighbors that are ON, it
+    turns OFF.
+    - If a cell is ON and has more than three neighbors that are ON, it
+    turns OFF.
+    - If a cell is ON and has either two or three neighbors that are ON,
+    it remains ON.
+    - If a cell is OFF and has exactly three neighbors that are ON, it
+    turns ON.
+    """
+
+    def __init__(self, size, starting_cells):
+        self.size = size
+        self.starting_cells = list(starting_cells)
+        self.active_cells = list(starting_cells)
+
+    def neighbors(self, cell):
+        """
+        return the list of ON cells around cell
+        """
+        neighbors = []
+        x, y = cell
+        for xpos in range(x-1, x+2):
+            for ypos in range(y-1, y+2):
+                if (xpos, ypos) == cell:
+                    continue
+                if (xpos, ypos) in self.active_cells:
+                    neighbors.append((xpos, ypos))
+
+        return neighbors
